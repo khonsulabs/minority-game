6de8025ab64c56b7f23873ba0ed66bd92136a085
kl-botsu <botsu@khonsulabs.com>
2022-02-04 14:49:22 +0000

    Deploying to gh-pages from @ khonsulabs/minority-game@661dc1b24efe4fc12bfad16e7927d46a7cd6042a 🚀

---
+++ main/implementors/bonsaidb_core/schema/view/trait.CollectionViewSchema.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["minority_game_server"] = [{"text":"impl CollectionViewSchema for <a class=\"struct\" href=\"minority_game_server/schema/struct.PlayerByScore.html\" title=\"struct minority_game_server::schema::PlayerByScore\">PlayerByScore</a>","synthetic":false,"types":["minority_game_server::schema::PlayerByScore"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
